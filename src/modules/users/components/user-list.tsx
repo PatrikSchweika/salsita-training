6403a8365a7671ef22023b17b1e6c6152aa3eca2
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-01 17:19:40 +0100

    Updated UserList component

--- src/modules/users/components/user-list.tsx
+++ src/modules/users/components/user-list.tsx
@@ -1,28 +1,18 @@
-import React, {FC, useState} from 'react';
-import {User} from "../user-types";
+import React, {FC} from 'react';
+import {AddUser, User} from "../user-types";
 
-export const UserList: FC = () => {
-
-    const [users, setUsers] = useState<User[]>([])
-
-    const addUser = (firstName: string, lastName: string) => {
-        const userToAdd : User = {
-            firstName: firstName,
-            lastName: lastName,
-            id: users.length
-        };
-
-        setUsers([...users, userToAdd]);
-    }
-
-    return (
-        <>
-            <button onClick={() => addUser('Arya', 'Stark')}>Add No One</button>
-            <button onClick={() => addUser('Daenerys', 'Targaryen')}>Add Mother of Dragons</button>
-            {users.length > 0 ? users.map(user =>
-                <li>{user.firstName} {user.lastName}</li>) :
-                <div>No Users</div>
-            }
-        </>
-    )
+interface UserListProps {
+    users: User[];
+    addUser: AddUser;
 }
+
+export const UserList: FC<UserListProps> = ({users, addUser}) => (
+    <>
+        <button onClick={() => addUser({firstName: 'Arya', lastName: 'Stark'})}>Add No One</button>
+        <button onClick={() => addUser({firstName: 'Daenerys', lastName: 'Targaryen'})}>Add Mother of Dragons</button>
+        {users.length > 0 ? users.map(user =>
+            <li>{user.firstName} {user.lastName}</li>) :
+            <div>No Users</div>
+        }
+    </>
+)
