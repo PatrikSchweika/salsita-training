e7d6a7b3cb83df790f877a40f730a582e91c9395
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-01 17:20:41 +0100

    Added state to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Root } from './modules/root/components/root';
+import {AddUser, User, UserName} from "./modules/users/user-types";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+interface State {
+    readonly title: string;
+    readonly users: User[];
+}
+
+let state : State = {
+    title: 'User Management',
+    users: []
+}
+
+const addUser: AddUser = (userName: UserName) => {
+    const userToAdd = {
+        ...userName,
+        id: state.users.length
+    };
+
+    state = {
+        ...state,
+        users: [...state.users, userToAdd]
+    };
+
+    render();
+}
+
+const render = () => ReactDOM.render(
+    <React.StrictMode>
+        <Root title={state.title} users={state.users} addUser={addUser} />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+render();
