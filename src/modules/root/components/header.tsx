c33a07b36dec8e90576beadc0be607608e97f614
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-01 17:19:29 +0100

    Added header variants

--- src/modules/root/components/header.tsx
+++ src/modules/root/components/header.tsx
@@ -1,10 +1,37 @@
 import React, {FC} from 'react';
 
 interface HeaderProps {
-    title: string
+    title: string;
 }
 
-export const Header: FC<HeaderProps> = ({ title }) => (
-    <h1>{title}</h1>
-)
+// Component
+// export class Header extends React.Component<HeaderProps, any> {
+//     render = () => {
+//         console.log('Header is rendering...')
+//         return <h1>{this.props.title}</h1>;
+//     }
+// }
+
+// PureComponent
+// export class Header extends React.PureComponent<HeaderProps, any> {
+//     render = () => {
+//         console.log('Header is rendering...')
+//         return <h1>{this.props.title}</h1>;
+//     }
+// }
+
+// Function
+// export const Header: FC<HeaderProps> = ({ title }) =>
+// {
+//     console.log('Header is rendering...');
+//     return <h1>{title}</h1>;
+// }
+
+// Function Memo
+export const Header = React.memo(({ title } : HeaderProps) =>
+{
+    console.log('Header is rendering...');
+    return <h1>{title}</h1>;
+})
+
 
