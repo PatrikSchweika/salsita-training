18c3a8768e1f30f9313daaa3488356813b3292c3
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-02 15:46:56 +0100

    Added server

---
+++ backend/server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import {User} from "../src/modules/users/user-types";
+
+const port = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let users: User[] = [{id: 0, firstName: 'Donald', lastName: 'Trump'}];
+
+app.get('/users', (req, res) => {
+   res.json(users);
+});
+
+app.post('/users', (req, res) => {
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+
+   if (firstName === undefined || lastName === undefined) {
+      res.sendStatus(400);
+   }
+   else {
+      const newUser: User = {
+         id: users.length,
+         firstName: firstName,
+         lastName: lastName
+      };
+
+      users = [...users, newUser];
+
+      res.json(newUser);
+   }
+});
+
+app.listen(port, () =>
+    console.log(`The server is running on port ${port}!`)
+)
