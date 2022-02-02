71088c361c382decc460c707b3dce68b1f6c3be8
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-02 12:22:08 +0100

    Updated index

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Root} from './modules/root/components/root';
-import {AddUser, UserName} from "./modules/users/user-types";
-import {compose, createStore} from "redux";
 import {rootReducer} from "./modules/root/root-reducer";
-import {UserActionCreators} from "./modules/users/user-actions";
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
 
-declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION__?: typeof compose
-    }
-}
+const store = configureStore({
+    reducer: rootReducer
+});
 
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ReactDOM.render(
+    <Provider store={store}>
+        <Root />
+    </Provider>,
+    document.getElementById('root')
 );
-
-const dispatchUser: AddUser = (userName: UserName) => {
-    store.dispatch(UserActionCreators.addUser(userName))
-}
-
-const render = () => {
-    const state = store.getState();
-
-    ReactDOM.render(
-        <React.StrictMode>
-            <Root title={state.title.title} users={state.users.users} addUser={dispatchUser} />
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-store.subscribe(render);
-
-render();
