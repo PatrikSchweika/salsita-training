b07ec2379ace969253399436b6aa5f64f1d1aac0
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-02 10:45:26 +0100

    Updated index

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Root } from './modules/root/components/root';
-import {AddUser, User, UserName} from "./modules/users/user-types";
+import {Root} from './modules/root/components/root';
+import {AddUser, UserName} from "./modules/users/user-types";
+import {compose, createStore} from "redux";
+import {rootReducer} from "./modules/root/root-reducer";
+import {UserActionCreators} from "./modules/users/user-actions";
 
 interface State {
     readonly title: string;
-    readonly users: User[];
 }
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION__?: typeof compose
+    }
+}
+
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 let state : State = {
-    title: 'User Management',
-    users: []
+    title: 'User Management'
 }
 
-const addUser: AddUser = (userName: UserName) => {
-    const userToAdd = {
-        ...userName,
-        id: state.users.length
-    };
+store.subscribe(() => render());
 
-    state = {
-        ...state,
-        users: [...state.users, userToAdd]
-    };
-
-    render();
+const dispatchUser: AddUser = (userName: UserName) => {
+    store.dispatch(UserActionCreators.addUser(userName))
 }
 
-const render = () => ReactDOM.render(
-    <React.StrictMode>
-        <Root title={state.title} users={state.users} addUser={addUser} />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const render = () => {
+    const users = store.getState().users.users;
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <Root title={state.title} users={users} addUser={dispatchUser} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 render();
