d152569f674675c44ac8522a897d5f040c853140
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-02 12:21:35 +0100

    Added export to RootState

--- src/modules/root/root-reducer.ts
+++ src/modules/root/root-reducer.ts
@@ -1,10 +1,10 @@
 import {combineReducers} from "redux";
-import {usersReducer} from "../users/users-reducer";
-import {titleReducer} from "../title/title-reducer";
+import {usersReducer} from "../users/users-slice";
+import {titleReducer} from "../title/title-slice";
 
 export const rootReducer = combineReducers({
     users: usersReducer,
     title: titleReducer
 })
 
-type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>;
