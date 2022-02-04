2a430042434f2c840fc7a8e4382e897bebdd66ce
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-04 14:59:30 +0100

    Created normalized user types

---
+++ src/modules/entities/normalized-user-types.ts
@@ -0,0 +1,23 @@
+import {Skill, UserName} from "../users/user-types";
+
+export type NormSkill = Skill;
+
+export interface NormUser extends UserName {
+    id: string;
+    regnalNumber: number;
+    skills: string[];
+}
+
+export interface NormUserSkill {
+    id: string;
+    skill: string;
+    level: number;
+}
+
+export interface UserEntities {
+    skills: {[key: string]: NormSkill};
+    userSkills: {[key: string]: NormUserSkill};
+    users: {[key: string]: NormUser};
+}
+
+export type UserIds = string[];
