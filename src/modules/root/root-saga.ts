e79417c82cfa931673efb96728b70ed2d83f9e49
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-08 13:42:40 +0100

    Updated root saga

--- src/modules/root/root-saga.ts
+++ src/modules/root/root-saga.ts
@@ -1,6 +1,8 @@
 import {fork} from "redux-saga/effects";
 import {usersSaga} from "../users/users-saga";
+import {usersRouteSaga} from "../users/users-route-saga";
 
 export function* rootSaga() {
-    yield fork(usersSaga)
+    yield fork(usersRouteSaga);
+    yield fork(usersSaga);
 }
