3e5fd0e2810ae1087f0bb19ae7bce99e2cc0b251
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-08 13:40:11 +0100

    Added router reducer to root

--- src/modules/root/root-reducer.ts
+++ src/modules/root/root-reducer.ts
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import {usersReducer} from "../users/users-slice";
 import {titleReducer} from "../title/title-slice";
 import {entitiesReducer} from "../entities/entities-slice";
+import {router5Reducer} from "redux-router5";
 
 export const rootReducer = combineReducers({
     users: usersReducer,
     title: titleReducer,
-    entities: entitiesReducer
+    entities: entitiesReducer,
+    router: router5Reducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>;
