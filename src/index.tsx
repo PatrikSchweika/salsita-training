ebcd8876796776945ada9de9f2b0dae3a34b185c
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-08 13:39:20 +0100

    Added router setup to index file

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,27 @@ import {Root} from './modules/root/components/root';
 import {rootReducer} from "./modules/root/root-reducer";
 import {configureStore} from "@reduxjs/toolkit";
 import {Provider} from "react-redux";
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
+import createRouter from 'router5';
 import {rootSaga} from "./modules/root/root-saga";
+import {router5Middleware} from "redux-router5";
+import {routes} from "./modules/router/routes";
+import browserPlugin from 'router5-plugin-browser';
+
+const router = createRouter(routes);
+router.usePlugin(browserPlugin());
+
+const routerMiddleware = router5Middleware(router);
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: [sagaMiddleware]
+    middleware: [sagaMiddleware, routerMiddleware]
 });
 
 sagaMiddleware.run(rootSaga);
+router.start();
 
 ReactDOM.render(
     <Provider store={store}>
