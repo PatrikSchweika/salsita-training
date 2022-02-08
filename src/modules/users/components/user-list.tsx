d4c9746d30c8c1665a1feb220b78ca189ce4f395
Patrik Schweika <patrik.schweika@gmail.com>
2022-02-08 13:42:30 +0100

    Updated UserList component

--- src/modules/users/components/user-list.tsx
+++ src/modules/users/components/user-list.tsx
@@ -4,6 +4,8 @@ import {usersActions} from "../users-slice";
 import {getUsersList} from "../users-selectors";
 import {MemoButton} from "../../root/components/memo-button";
 import {User} from "../user-types";
+import {RouterLink} from "../../root/components/router-link";
+import {userDetailRoute} from "../../router/routes";
 
 export const UserList: FC = () => {
     const userList: User[] = useSelector(getUsersList);
@@ -23,14 +25,15 @@ export const UserList: FC = () => {
     <>
         <MemoButton onClick={addUserOneCallback}>Add No one</MemoButton>
         <MemoButton onClick={addUserTwoCallback}>Add Mother of Dragons</MemoButton>
-
+        <ul>
         {userList.length > 0 ?
             userList.map(user =>
-                <li key={user.id}>{user.firstName} {user.regnalNumber}. {user.lastName}
+                <li key={user.id}><RouterLink routeName={userDetailRoute} params={{'id': user.id}}>{user.firstName} {user.regnalNumber}. {user.lastName}</RouterLink>
                     {user.skills.map(skill => <div key={skill.skill.id}>Skill: {skill.skill.name} Level: {skill.level}</div>)}
                 </li>) :
             <div>No Users</div>
         }
+        </ul>
     </>
     );
 }
